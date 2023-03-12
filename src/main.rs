50c9d7e4e4da46938bfda6e2712721dcdd78ab25
Default Cube <56493793+Quicksilver151@users.noreply.github.com>
2023-03-12 08:40:11 +0500

    Format code

--- src/main.rs
+++ src/main.rs
@@ -2,27 +2,25 @@
 pub use std::env;
 pub use std::fs;
 
-
-// crates 
+// crates
 pub use home::*;
-pub use serde::{Serialize,Deserialize};
 pub use owo_colors::*;
+pub use serde::{Deserialize, Serialize};
 
 // files
-mod structs;
-mod parsers;
 mod apps;
+mod parsers;
+mod structs;
 
 // use files
-use structs::*;
-use parsers::*;
 use apps::*;
-
+use parsers::*;
+use structs::*;
 
 fn main() {
     // init
     // handle_ctrlc();
-    
+
     // load config
     let cfg_result: Result<Config, confy::ConfyError> = confy::load("polywal", None);
     let cfg = match cfg_result {
@@ -33,31 +31,27 @@ fn main() {
             Config::new()
         }
     };
-       
+
     confy::store("polywal", None, cfg).unwrap();
-    
+
     // fetch flags
     let args: Vec<String> = env::args().collect();
     let flag: Flag = match args::parse_args(args) {
         Ok(flag) => flag,
         Err(_flag) => return,
     };
-    
+
     // help break
     if flag.help {
         // breakout for help
         println!("{}", HELP_TEXT);
         return;
     }
-    
-    
+
     // main logic
     // ==========
-    
-    
-    let wal_colors:Palette = get_hex_colors();
+
+    let wal_colors: Palette = get_hex_colors();
     write_palette_to(App::Tabliss, &wal_colors);
     println!("{}", &wal_colors);
-    
-    
 }
