e58dfa195646916a0427ea1dd7085a865c28b339
Default Cube <56493793+Quicksilver151@users.noreply.github.com>
2023-03-15 02:51:11 +0500

    Removed warning

--- src/main.rs
+++ src/main.rs
@@ -52,6 +52,6 @@ fn main() {
     // ==========
     
     let wal_colors: Palette = get_hex_colors();
-    write_palette_to(App::Tabliss, &wal_colors);
+    write_palette_to(App::Tabliss, &wal_colors).unwrap_or(());
     println!("{}", &wal_colors);
 }
