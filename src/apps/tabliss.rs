2f5071958657ec4d9c838b531d4addbd5111d2a1
Default Cube <56493793+Quicksilver151@users.noreply.github.com>
2023-03-23 17:37:32 +0500

    Finally done with tabliss

--- src/apps/tabliss.rs
+++ src/apps/tabliss.rs
@@ -1,9 +1,18 @@
+
 use crate::*;
 
 pub fn set_theme() -> Result<(), FileError>{
     let path = get_relative_path(
         ".mozilla/firefox/nvrfmscc.default-release/storage/default/moz-extension+++f05e6875-9650-477d-90ba-a753d8509a85^userContextId=4294967295/idb/3647222921wleabcEoxlt-eengsairo.files/4");
     
-    println!("tabliss path:\n{}",path.to_str().unwrap());
+    let wallpaper = get_wallaper_path();
+    dbg!(&wallpaper);
+    fs::remove_file(&path).unwrap();
+    dbg!(&path);
+    std::os::unix::fs::symlink(&wallpaper, path).unwrap();
+
+    // fs::soft_link(path, link).unwrap_or(());
+    
+    // println!("tabliss path:\n{}",path.to_str().unwrap());
     Ok(())
 }
--- src/parsers/wal_output.rs
+++ src/parsers/wal_output.rs
@@ -1,23 +1,35 @@
 use crate::*;
 
+pub fn file_content_from_wal(dir:&str) -> String {
+    let mut file_path = home_dir().unwrap();
+    file_path.push(".cache/wal/");
+    file_path.push(dir);
+    
+    fs::read_to_string(file_path).expect("Should have been able to read the file")
+}
+
+pub fn file_content_from_home(dir:&str) -> String {
+    let mut file_path = home_dir().unwrap();
+    file_path.push(dir);
+    
+    fs::read_to_string(file_path).expect("Should have been able to read the file")
+}
+
+
 pub fn get_hex_colors() -> Palette {
     let mut file_path = home_dir().unwrap();
-    file_path.push(".cache/wal/colors");
+    file_path.push("colors");
     
-    let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
-    
+    let contents = file_content_from_home(".cache/wal/colors");
     let contents: Vec<&str> = contents.split('\n').collect();
     
     Palette::from_vec(contents)
 }
 
-pub fn get_wallaper_location() -> String{
-    todo!();
+pub fn get_wallaper_path() -> String{
+    file_content_from_wal("wal")
 }
 
 pub fn get_256_colors() {
     todo!()
-}
-pub fn get_wallpaper_path() -> String {
-    todo!();
 }
